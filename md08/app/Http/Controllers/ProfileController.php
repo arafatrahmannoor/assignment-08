0a725c93eb83cf3b1af4b03939df07360e27ada2
arafatrahmannoor <rahman15-6011@diu.edu.bd>
2023-11-20 00:03:52 +0600

    8th Modeule

---
+++ md08/app/Http/Controllers/ProfileController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Cookie;
+
+class ProfileController extends Controller
+{
+    public function index($id)
+    {
+       
+        $name = "Donald Trump";
+        $age = "75";
+
+        
+        $data = [
+            'id' => $id,
+            'name' => $name,
+            'age' => $age,
+        ];
+
+        
+        $cookieName = 'access_token';
+        $cookieValue = '123-XYZ';
+        $minutes = 1;
+        $path = '/';
+        $domain = $_SERVER['SERVER_NAME'];
+        $secure = false;
+        $httpOnly = true;
+
+       
+        $response = new Response($data, 200);
+        $response->withCookie(Cookie::make($cookieName, $cookieValue, $minutes, $path, $domain, $secure, $httpOnly));
+
+        return $response;
+    }
+}
